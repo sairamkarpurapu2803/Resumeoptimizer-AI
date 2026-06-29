ec91c26d833f59ec6478532350cb34a94c2f3c53
sairam karpurapu <sairamkarpurapu2803@gmail.com>
2026-06-29 11:53:09 +0530

    Update install_kubernetes.sh

--- scripts/install_kubernetes.sh
+++ scripts/install_kubernetes.sh
@@ -1,5 +1,5 @@
-
 #!/bin/bash
+set -e
 
 sudo apt update
 
@@ -7,9 +7,11 @@ sudo apt install -y apt-transport-https ca-certificates curl gpg
 
 sudo mkdir -p /etc/apt/keyrings
 
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key \
+| sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes.gpg
 
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" \
+| sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt update
 
