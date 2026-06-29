9aa7acd7f3404e735b8097812d2dd2213bfc03d7
sairam karpurapu <sairamkarpurapu2803@gmail.com>
2026-06-29 14:05:04 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,7 @@
+import axios from "axios";
 
-import axios from 'axios';
-
-export default axios.create({
-baseURL:"http://localhost:8000"
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL
 });
+
+export default api;
